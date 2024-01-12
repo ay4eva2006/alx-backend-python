26a890db956811c10e9044abbe8587a6cbcb47be
Ayo John Adekunle <ay4eva2006@gmail.com>
2024-01-12 20:34:44 +0100

    advance task2 adjustment2

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-""" a python script to annonate a typeVar"""
+""" A python script to annonate a TypeVar"""
 from typing import TypeVar, Mapping, Any, Union
 
 T = TypeVar('T')
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
-    """function to return a typeVar"""
+
+def safely_get_value(
+        dct: Mapping, key: Any, default: Union[T, None] = None
+) -> Union[Any, T]:
+    """function to return a dictionary"""
     if key in dct:
         return dct[key]
     else:
-       return default
-
+        return default
