c99eaeeb1e82186c24a897771e49799d1dd95849
Ayo John Adekunle <ay4eva2006@gmail.com>
2024-01-12 16:38:59 +0100

    advance task10

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from typing import Any, Optional
+""" a python script that Safely returns the first element of an iterable or None if the iterable is empty.
+
+    Parameters:
+    - lst: The input iterable.
+
+   Returns:
+    - any: The first element of the iterable if it exists, otherwise None.
+    """
+def safe_first_element(lst) -> Optional[Any]:
+    if lst:
+        return lst[0]
+    else:
+        return None
