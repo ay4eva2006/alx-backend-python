5bfef4230b34bd1c63c0d40c895141ee8f3a9755
Ayo John Adekunle <ay4eva2006@gmail.com>
2024-01-12 17:36:14 +0100

    advance task10 adjust2

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-from typing import Sequence, Any, Union, Optional
-""" a python script that Safely returns the first element of an iterable or None if the iters, otherwise None."""
+""" a python script to annonate any type"""
+from typing import Sequence, Any, Union
+
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """function to return a any or None type"""
     if lst:
         return lst[0]
     else:
