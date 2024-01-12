0d2e426941bced7d6087667ffe7956504819ace0
Ayo John Adekunle <ay4eva2006@gmail.com>
2024-01-12 20:48:30 +0100

    advance task adjustment

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" a python script to annonate a sequence"""
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
+    zoomed_in: List[int] = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+"""Fixing the type of factor to be an int"""
+
+zoom_3x = zoom_array(array, 3)
