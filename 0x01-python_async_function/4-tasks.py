75628776606a82cc750baca63917ff3ec5f8eb7a
Ayo John Adekunle <ay4eva2006@gmail.com>
2024-01-15 21:08:07 +0100

    task4 adjust

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+"""function to use task_wait_random"""
 import asyncio
 from typing import List
 from heapq import nsmallest
 task_wait_random = __import__('3-tasks').task_wait_random
-"""function to use task_wait_random"""
+
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
