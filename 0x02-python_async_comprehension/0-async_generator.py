9395eb68adb7d1f8872b0115d6a3aa47d05d31b0
Ayo John Adekunle <ay4eva2006@gmail.com>
2024-01-16 12:15:12 +0100

    Async comprehension task 0 and 1 debug pycodestyle

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """A Function to generate async generator using comprehension """
-import asyncio, random
+import asyncio
+import random
 from typing import Generator
 
 
-async def async_generator() ->  Generator[float,None, None]:
+sync def async_generator() -> Generator[float, None, None]:
     """A function that will loop 10 times, wait 1 sec. each time"""
     for i in range(10):
         await asyncio.sleep(1)
--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -5,6 +5,9 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """A coroutine that collect 10 ramdom numbers(return floats type list) using async comprehension"""
+    """
+    A coroutine that collect 10 ramdom numbers
+    (return floats type list) using async comprehension
+    """
     results = [i async for i in async_generator()]
     return results
